85f136cf235d08becd4a1d240389098d78cad142
nicklastrange <mikolaj.cekut@gmail.com>
2021-12-05 13:51:07 +0100

    Added controller and it's logic

---
+++ src/main/java/pl/umcs/clinicmanager/receipt/ReceiptController.java
@@ -0,0 +1,44 @@
+package pl.umcs.clinicmanager.receipt;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class ReceiptController {
+
+    private final ReceiptService receiptService;
+
+    @PostMapping("/receipt/add")
+    ResponseEntity(Receipt) addReceipt(@RequestBody ReceiptDTO newReceipt) {
+        final Receipt addedReceipt = receiptService.addReceipt(newReceipt);
+        return new ResponseEntity<>(addedReceipt, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/receipts/{doctorID}")
+    ResponseEntity<List<ReceiptDTO>> getAllReceiptsByDoctorID(@PathVariable Long doctorID) {
+        final List<ReceiptDTO> receipts = receiptService.getAllReceiptsByDoctorID(doctorID);
+        return new ResponseEntity<>(receipts, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/receipts/{patientID}")
+    ResponseEntity<List<ReceiptDTO>> getAllReceiptsByPatientID(@PathVariable Long patientID) {
+        final List<ReceiptDTO> receipts = receiptService.getAllReceiptsByPatientID(patientID);
+        return new ResponseEntity<>(receipts, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/receipt/{id}")
+    ResponseEntity<ReceiptDTO> getReceiptByID(@PathVariable Long id) {
+        final ReceiptDTO receipt = receiptService.getReceipt(id);
+        return new ResponseEntity<>(receipt, HttpStatus.FOUND);
+    }
+
+    @DeleteMapping("/receipt/delete/{id}")
+    ResponseEntity<Void> deleteReceipt(@PathVariable Long id) {
+        receiptService.deleteReceipt(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+}
