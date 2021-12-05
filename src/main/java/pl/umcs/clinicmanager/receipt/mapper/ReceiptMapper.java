b517541f1ad78aa70114f7dd992b9239bc0194ac
nicklastrange <mikolaj.cekut@gmail.com>
2021-12-05 14:37:34 +0100

    Created receipt mapper

---
+++ src/main/java/pl/umcs/clinicmanager/receipt/mapper/ReceiptMapper.java
@@ -0,0 +1,37 @@
+package pl.umcs.clinicmanager.receipt.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import pl.umcs.clinicmanager.receipt.domain.Receipt;
+import pl.umcs.clinicmanager.receipt.dto.ReceiptDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ReceiptMapper {
+
+    @Mapping(source = "receiptDescription", target = "description")
+    @Mapping(source = "receiptPrescriptionDate", target = "prescriptionDate")
+    @Mapping(source = "receiptPatient", target = "patient")
+    @Mapping(source = "receiptDoctor", target = "doctor")
+    Receipt dtoToEntity(ReceiptDTO receipt);
+
+
+    @Mapping(source = "description", target = "receiptDescription")
+    @Mapping(source = "prescriptionDate", target = "receiptPrescriptionDate")
+    @Mapping(source = "patient", target = "receiptPatient")
+    @Mapping(source = "doctor", target = "receiptDoctor")
+    ReceiptDTO entityToDto(Receipt receipt);
+
+    default List<ReceiptDTO> entityListToDtoList(List<Receipt> receiptList) {
+        if (receiptList == null) {
+            return null;
+        }
+        List<ReceiptDTO> mappedList = new ArrayList<>(receiptList.size());
+        for (Receipt receipt : receiptList) {
+            mappedList.add(entityToDto(receipt));
+        }
+        return mappedList;
+    }
+}
